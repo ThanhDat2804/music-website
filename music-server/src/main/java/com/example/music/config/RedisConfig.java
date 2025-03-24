bf50d0d0140a6a63eeb57c06738268e138df01d7
ThanhDat2804 <dat.le@savvycom.vn>
2025-03-24 16:30:13 +0700

    add security config

---
+++ music-server/src/main/java/com/example/music/config/RedisConfig.java
@@ -0,0 +1,53 @@
+package com.example.music.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.*;
+
+import java.time.Duration;
+
+@EnableCaching // Enable caching annotations
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+        GenericJackson2JsonRedisSerializer jsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
+
+        template.setConnectionFactory(factory);
+        // Key serialization method
+        template.setKeySerializer(redisSerializer);
+        // Value serialization
+        template.setValueSerializer(jsonRedisSerializer);
+        // HashMap value serialization
+        template.setHashValueSerializer(jsonRedisSerializer);
+
+        return template;
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory factory) {
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+        GenericJackson2JsonRedisSerializer jsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
+
+        // Configure serialization and set expiration time to 600 seconds
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(600))
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jsonRedisSerializer))
+                .disableCachingNullValues();
+
+        return RedisCacheManager.builder(factory)
+                .cacheDefaults(config)
+                .build();
+    }
+}
--- music-server/src/main/java/com/example/music/mapper/AdminMapper.java
+++ music-server/src/main/java/com/example/music/mapper/AdminMapper.java
@@ -4,7 +4,9 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.music.model.domain.Admin;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface AdminMapper extends BaseMapper<Admin> {
 
 }
+
