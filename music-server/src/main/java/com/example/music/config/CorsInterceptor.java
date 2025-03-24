4bec6e36cec3b256b82ad48de554a3b87a17d07a
ThanhDat2804 <dat.le@savvycom.vn>
2025-03-24 17:00:23 +0700

    hot-fix: renaming

--- music-server/src/main/java/com/example/music/config/CorsInterceptor.java
+++ music-server/src/main/java/com/example/music/config/CorsInterceptor.java
@@ -1,8 +1,8 @@
 package com.example.music.config;
 
-import org.springframework.web.servlet.HandlerInterceptor;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
 
 public class CorsInterceptor implements HandlerInterceptor {
 
